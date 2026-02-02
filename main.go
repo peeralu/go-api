8e9df37255262de0de751b916d911ac908c9d178
Peerawat Luekachon <peerawat.lu@gmail.com>
2026-02-02 23:34:54 +0700

    feat: init

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/adaptor"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const (
+	logFilePath = "/var/log/app/app.log"
+	serverPort  = ":8000"
+)
+
+var (
+	httpRequestsTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"method", "path", "status"},
+	)
+
+	httpRequestDuration = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_duration_seconds",
+			Help:    "Duration of HTTP requests",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"method", "path"},
+	)
+)
+
+func newLogger() (*zap.Logger, func(), error) {
+	logFile, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.TimeKey = "timestamp"
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	encoder := zapcore.NewJSONEncoder(encoderConfig)
+
+	core := zapcore.NewTee(
+		zapcore.NewCore(encoder, zapcore.AddSync(logFile), zap.InfoLevel),
+		zapcore.NewCore(encoder, zapcore.AddSync(os.Stdout), zap.InfoLevel),
+	)
+
+	logger := zap.New(core)
+
+	cleanup := func() {
+		logger.Sync()
+		logFile.Close()
+	}
+
+	return logger, cleanup, nil
+}
+
+func prometheusMiddleware() fiber.Handler {
+	return func(c fiber.Ctx) error {
+		timer := prometheus.NewTimer(httpRequestDuration.WithLabelValues(c.Method(), c.Path()))
+		defer timer.ObserveDuration()
+
+		err := c.Next()
+
+		httpRequestsTotal.WithLabelValues(c.Method(), c.Path(), string(rune(c.Response().StatusCode()))).Inc()
+
+		return err
+	}
+}
+
+func setupRoutes(app *fiber.App, logger *zap.Logger) {
+	app.Get("/metrics", adaptor.HTTPHandler(promhttp.Handler()))
+
+	app.Get("/", func(c fiber.Ctx) error {
+		logger.Info("Request received", zap.String("path", "/"))
+		return c.SendString("Hello, World!")
+	})
+}
+
+func main() {
+	logger, cleanup, err := newLogger()
+	if err != nil {
+		panic(err)
+	}
+	defer cleanup()
+
+	app := fiber.New()
+	app.Use(prometheusMiddleware())
+	setupRoutes(app, logger)
+
+	logger.Info("Starting server", zap.String("port", serverPort))
+	if err := app.Listen(serverPort); err != nil {
+		logger.Fatal("Failed to start server", zap.Error(err))
+	}
+}
